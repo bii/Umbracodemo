d7b645b91edaf2a6aa53e31d214a88ccfcb7fa0d
Bilal Isa <isabilal@gmail.com>
2019-04-09 08:30:48 +0200

    first commit

---
+++ UmbracoDemo/Models/CustomLink.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace UmbracoDemo.Models
+{
+    public class CustomLink
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("target")]
+        public string Target { get; set; }
+
+        public CustomLink Deserialize(string json)
+        {
+            // Validate the JSON
+            if (json == null || !json.StartsWith("{") || !json.EndsWith("}"))
+            {
+                return null;
+            }
+
+            // Deserialize the JSON
+            var jobj = (JObject)JsonConvert.DeserializeObject(json);
+
+            return new CustomLink
+            {
+                Id = (int)jobj["id"],
+                Name = (string)jobj["name"],
+                Url = (string)jobj["url"],
+                Target = (string)jobj["target"]
+            };
+        }
+    }
+}
---
+++ UmbracoDemo/PropertyValueConverters/CustomLinkPickerPropertyValueConverter.cs
@@ -0,0 +1,31 @@
+﻿using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+using UmbracoDemo.Models;
+
+namespace UmbracoDemo.PropertyValueConverters
+{
+    public class CustomLinkPickerPropertyValueConverter : IPropertyValueConverter
+    {
+        private readonly CustomLink customLink = new CustomLink();
+
+        public object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source;
+        }
+
+        public object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return customLink.Deserialize(source as string);
+        }
+
+        public object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return null;
+        }
+
+        public bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias == "custom.link.picker";
+        }
+    }
+}
